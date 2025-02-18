2b7239348e1d4d201c960d99c325b190cb60177a
Marcelo Augusto <Marceloaugustocge@gmail.com>
2025-02-17 22:02:53 -0300

    modified:   aula-01.1/app.py 	new file:   aula-01.1/controllers/__pycache__/routes.cpython-312.pyc 	new file:   aula-01.1/controllers/routes.py 	new file:   aula-01.1/views/cad_games.html 	modified:   aula-01.1/views/games.html 	modified:   aula-01.1/views/index.html

--- aula-01.1/app.py
+++ aula-01.1/app.py
@@ -1,25 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+from controllers import routes
+import requests
 
-#criando a instancia do flask na variavel do app
-app = Flask(__name__,template_folder='views')
+# criando a instancia do flask na variavel do app
+app = Flask(__name__, template_folder='views')
 
-#criando a primeira rota da aplicação
-
-@app.route('/')
-#view function -> função de visualização
-def home():
-    return render_template('index.html')
-
-#segunda rota 
-
-@app.route('/games')
-def games():
-    return render_template('games.html')
-
-#terceira rota
-    
-
-#iniciar o servidor
+routes.init_app(app)
 
 if __name__ == '__main__':
-    app.run(host="localhost",port=5000,debug=True)
+    app.run(host="localhost", port=5000, debug=True)
---
+++ aula-01.1/controllers/routes.py
@@ -0,0 +1,31 @@
+from flask import render_template, request, redirect, url_for
+jogadores = []
+gamelist = [{"Titulo": "CS-GO",
+         "Ano": 2017,
+          "Categoria": "FPS-Online"
+         }, {
+             
+         }]
+
+
+def init_app(app):
+    # criando a primeira rota da aplicação
+    @app.route('/')
+    # view function -> função de visualização
+    def home():
+        return render_template('index.html')
+
+    @app.route('/games', methods=['POST', 'GET'])
+    def games():
+        games = gamelist[0]
+        if request.method == 'POST':
+            nome_jogador = request.form.get('Jogador')
+            jogadores.append(nome_jogador)
+            return redirect(url_for('games'))
+
+        return render_template('games.html', games=games, jogadores=jogadores)
+
+    @app.route('/cad_games', methods=['GET', 'POST'])
+    def cad_games():
+        
+        return render_template('cad_games.html')
